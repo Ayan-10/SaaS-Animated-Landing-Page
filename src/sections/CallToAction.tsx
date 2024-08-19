6d3b6855f2c4b7cfb8d711aa00195ac56167c3f1
ayans <ayans@synopsys.com>
2024-08-19 12:21:04 +0530

    first commit

---
+++ src/sections/CallToAction.tsx
@@ -0,0 +1,17 @@
+export const CallToAction = () => {
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container">
+        <h2 className="section-title">Sign up for free today</h2>
+        <p className="section-description mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts.
+        </p>
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text">Learn more</button>
+        </div>
+      </div>
+    </section>
+  );
+};
