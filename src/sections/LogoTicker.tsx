4b4cf02e44ed9edab3a51645d03e7a1c0439d88a
ayans <ayans@synopsys.com>
2024-08-21 10:29:33 +0530

    Till pricing animation

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,17 +1,36 @@
-import acmeLogo  from "@/assets/logo-acme.png";
-import quantumLogo  from "@/assets/logo-quantum.png";
+"use client";
+
+import acmeLogo from "@/assets/logo-acme.png";
+import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
 import celestrialLogo from "@/assets/logo-celestial.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import apexLogo from "@/assets/logo-apex.png";
 import Image from "next/image";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
 
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
             <Image
               src={acmeLogo}
               alt="Acme Logo"
@@ -42,7 +61,39 @@ export const LogoTicker = () => {
               alt="Acme Logo"
               className="logo-ticker-image"
             />
-          </div>
+
+            {/* {For second time loop} */}
+            <Image
+              src={acmeLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={quantumLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={echoLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={celestrialLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={pulseLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+            <Image
+              src={apexLogo}
+              alt="Acme Logo"
+              className="logo-ticker-image"
+            />
+          </motion.div>
         </div>
       </div>
     </div>
--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,12 +1,30 @@
-import productImage from '@/assets/product-image.png';
-import Image from 'next/image';
+"use client";
+
+import productImage from "@/assets/product-image.png";
+import Image from "next/image";
 import pyramidImage from "@/assets/pyramid.png";
 import tubeImage from "@/assets/tube.png";
-
+import {
+  motion,
+  useScroll,
+  useTransform,
+  useMotionValueEvent,
+} from "framer-motion";
+import { useRef } from "react";
 
 export const ProductShowcase = () => {
+  const sectRef = useRef(null);
+  const {scrollYProgress} = useScroll({
+    target: sectRef,
+    offset: ['start end', 'end start']
+  })
+
+  const translateY = useTransform(scrollYProgress, [0,1], [150, -150])
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
+    <section
+      ref={sectRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip"
+    >
       <div className="container">
         <div className="section-heading">
           <div className="flex justify-center">
@@ -23,18 +41,25 @@ export const ProductShowcase = () => {
         </div>
         <div className="relative">
           <Image src={productImage} alt="Product Image" className="mt-10" />
-          <Image
-            src={pyramidImage}
+          <motion.img
+            src={pyramidImage.src}
             alt="pyramoid Image"
             className="hidden md:block absolute -right-36 -top-32 "
             height={262}
             width={262}
+            style={{
+              translateY: translateY,
+            }}
           />
-          <Image
-            src={tubeImage}
+          <motion.img
+            src={tubeImage.src}
             alt="tube Image"
-            className="hidden md:block absolute bottom-24 -left-36"
             height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY: translateY,
+            }}
           />
         </div>
       </div>
